831f65d015d07431fad136564383aeffad01473c
Brad Wadsworth <bwadsworth@digitalmeasures.com>
2016-08-04 15:16:36 -0500

    cleanup create certs script

--- create-certs.sh
+++ create-certs.sh
@@ -12,14 +12,18 @@ CSR=/etc/ssl/csr/cert.csr
 create_ca () {
   if [ ! -f "${CA_ROOT_KEY}" ]; then
     echo
-    echo "Generating CA root key."
+    echo "----------------------------------------------------------------------"
+    echo "Generating CA root key at ${CA_ROOT_KEY}."
+    echo "----------------------------------------------------------------------"
     echo
     openssl genrsa -aes256 -out "${CA_ROOT_KEY}" 4096
   fi
 
   if [ ! -f "${CA_ROOT_CERT}" ]; then
     echo
-    echo "Generating CA cert."
+    echo "----------------------------------------------------------------------"
+    echo "Generating CA cert at ${CA_ROOT_CERT}."
+    echo "----------------------------------------------------------------------"
     echo
     openssl req -config /etc/ssl/openssl.cnf \
       -key "${CA_ROOT_KEY}" \
@@ -32,7 +36,9 @@ create_intermediate () {
   if [ ! -f "${CA_INTERMEDIATE_KEY}" ]
   then
     echo
-    echo "Creating CA intermediate key."
+    echo "----------------------------------------------------------------------"
+    echo "Generating CA intermediate key at ${CA_INTERMEDIATE_KEY}."
+    echo "----------------------------------------------------------------------"
     echo
     openssl genrsa -aes256 \
       -out "${CA_INTERMEDIATE_KEY}" 4096
@@ -41,14 +47,18 @@ create_intermediate () {
   if [ ! -f "${CA_INTERMEDIATE_CERT}" ]
   then
     echo
-    echo "Creating CA intermediate csr."
+    echo "----------------------------------------------------------------------"
+    echo "Generating CA intermediate csr."
+    echo "----------------------------------------------------------------------"
     echo
     openssl req -config /etc/ssl/openssl.cnf -new -sha256 \
       -key "${CA_INTERMEDIATE_KEY}" \
       -out /etc/ssl/csr/intermediate.csr
 
     echo
-    echo "Signing CA intermediate certificate with root CA."
+    echo "-------------------------------------------------------------------------------"
+    echo "Signing CA intermediate certificate with root CA at ${CA_INTERMEDIATE_CERT}."
+    echo "-------------------------------------------------------------------------------"
     echo
     openssl ca -config /etc/ssl/openssl.cnf -extensions v3_intermediate_ca \
       -days 3650 -notext -md sha256 \
@@ -67,13 +77,17 @@ Enter the kind of certificate you would like to create:(s) " TYPE
 TYPE=${TYPE:-s}
 
 echo
+echo "----------------------------------------------------------------------"
 echo "Creating server key."
+echo "----------------------------------------------------------------------"
 echo
 openssl genrsa \
   -out "${KEY}" 2048
 
 echo
+echo "----------------------------------------------------------------------"
 echo "Creating certificate signing request."
+echo "----------------------------------------------------------------------"
 echo
 openssl req -config /etc/ssl/openssl.cnf \
   -key "${KEY}" \
@@ -82,7 +96,9 @@ openssl req -config /etc/ssl/openssl.cnf \
 case "${TYPE}" in
   s)
     echo
+    echo "----------------------------------------------------------------------"
     echo "Creating self-signed certificate."
+    echo "----------------------------------------------------------------------"
     echo
     openssl x509 -req -days 365 -in "${CSR}" \
       -signkey "${KEY}" -out "${CERT}"
@@ -90,7 +106,9 @@ case "${TYPE}" in
   c)
     create_ca
     echo
+    echo "----------------------------------------------------------------------"
     echo "Signing certificate signing request with CA certificate."
+    echo "----------------------------------------------------------------------"
     echo
     openssl ca -config /etc/ssl/openssl.cnf \
       -extensions server_cert -days 375 -notext -md sha256 \
@@ -98,20 +116,28 @@ case "${TYPE}" in
       -out "${CERT}"
 
     echo
-    echo "Here is your CA Key."
+    echo "----------------------------------------------------------------------"
+    echo "Here is your CA key at ${CA_ROOT_KEY}."
+    echo "----------------------------------------------------------------------"
     echo
     cat "${CA_ROOT_KEY}"
 
     echo
-    echo "Here is your CA certificate."
+    echo "----------------------------------------------------------------------"
+    echo "Here is your CA certificate at ${CA_ROOT_CERT}."
+    echo "----------------------------------------------------------------------"
     echo
+    echo "----------------------------------------------------------------------"
     cat "${CA_ROOT_CERT}"
+    echo "----------------------------------------------------------------------"
     ;;
   i)
     create_ca
     create_intermediate
     echo
+    echo "----------------------------------------------------------------------"
     echo "Signing certificate signing request with intermediate certificate."
+    echo "----------------------------------------------------------------------"
     echo
     openssl ca -config /etc/ssl/openssl.cnf -name CA_intermediate_dummy \
       -extensions server_cert -days 375 -notext -md sha256 \
@@ -119,24 +145,40 @@ case "${TYPE}" in
       -out "${CERT}"
 
    echo
-   echo "Here is your CA Key."
+   echo "----------------------------------------------------------------------"
+   echo "Here is your CA key at ${CA_ROOT_KEY}."
+   echo "----------------------------------------------------------------------"
    echo
+   echo "----------------------------------------------------------------------"
    cat "${CA_ROOT_KEY}"
+   echo "----------------------------------------------------------------------"
 
    echo
-   echo "Here is your CA certificate."
+   echo "----------------------------------------------------------------------"
+   echo "Here is your CA certificate at ${CA_ROOT_CERT}."
+   echo "----------------------------------------------------------------------"
    echo
+   echo "----------------------------------------------------------------------"
    cat "${CA_ROOT_CERT}"
+   echo "----------------------------------------------------------------------"
 
    echo
-   echo "Here is your intermediate Key."
+   echo "----------------------------------------------------------------------"
+   echo "Here is your intermediate key at ${CA_INTERMEDIATE_KEY}."
+   echo "----------------------------------------------------------------------"
    echo
+   echo "----------------------------------------------------------------------"
    cat "${CA_INTERMEDIATE_KEY}"
+   echo "----------------------------------------------------------------------"
 
    echo
-   echo "Here is your intermediate certificate."
+   echo "----------------------------------------------------------------------"
+   echo "Here is your intermediate certificate at ${CA_INTERMEDIATE_CERT}."
+   echo "----------------------------------------------------------------------"
    echo
+   echo "----------------------------------------------------------------------"
    cat "${CA_INTERMEDIATE_CERT}"
+   echo "----------------------------------------------------------------------"
    ;;
    *)
       echo
@@ -146,11 +188,19 @@ case "${TYPE}" in
  esac
 
 echo
+echo "----------------------------------------------------------------------"
 echo "Here is your server key."
+echo "----------------------------------------------------------------------"
 echo
+echo "----------------------------------------------------------------------"
 cat "${KEY}"
+echo "----------------------------------------------------------------------"
 
 echo
+echo "----------------------------------------------------------------------"
 echo "Here is your server certificate."
+echo "----------------------------------------------------------------------"
 echo
+echo "----------------------------------------------------------------------"
 cat "${CERT}"
+echo "----------------------------------------------------------------------"
